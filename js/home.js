b7d132c82f32fd12b7f01f9c6dc185ebd1908a96
Jeongpil <nmmcjp@gmail.com>
2021-11-14 12:17:07 -0800

    ready for slide action

--- js/home.js
+++ js/home.js
@@ -48,14 +48,33 @@
     })
     
     const container = document.querySelector('.heroPageSection')
-    const posterContainerWidth = window.innerWidth*layPoster.length + 'px'
+    const posterContainerWidth = window.innerWidth*(layPoster.length+2) + 'px'
     container.style.width=posterContainerWidth
+
 
     layPoster.forEach((poster)=>{
         container.innerHTML += poster
         addClass()
     })
+
+    const clonedFirstMovie = `
+    <div class="heroItem clonedFirstMovie" style='background-image: url(${heroMovies[0].image});'>
+    <div class="heroTextPart">
+        <p class="title">${heroMovies[0].title}</p>
+        <p class="subTitle">${heroMovies[0].text}</p>
+        <a href="#" class="orangeBtn"><span class="material-icons">
+            credit_card
+            </span>Buy tickets</a>
+
+        <a href="#" class="grayBtn"><span class="material-icons">
+            videocam
+            </span> Play trailer</a>
+        </div>
+    </div>  
+    `
+    // clonedFirstMovie.classList.add('clonedFirstMovie')
     container.innerHTML+=`
+    ${clonedFirstMovie}
           <div class="arrow prevBtn">
                 <span class="material-icons">
                     arrow_back_ios_new
@@ -72,8 +91,9 @@
                 <span class="dot"></span>
             </div>
     `
+   
 
-
+    
     function addClass(){
         let classNum = 1
         const posters = document.querySelectorAll('.heroItem')
@@ -83,6 +103,30 @@
         })
     }
 
+    const clonedLastMovie = `
+    <div class="heroItem clonedLastMovie" style='background-image: url(${heroMovies[heroMovies.length-1].image});'>
+    <div class="heroTextPart">
+        <p class="title">${heroMovies[heroMovies.length-1].title}</p>
+        <p class="subTitle">${heroMovies[heroMovies.length-1].text}</p>
+        <a href="#" class="orangeBtn"><span class="material-icons">
+            credit_card
+            </span>Buy tickets</a>
+
+        <a href="#" class="grayBtn"><span class="material-icons">
+            videocam
+            </span> Play trailer</a>
+        </div>
+    </div>  
+    `
+    const heroContents = container.innerHTML
+    container.innerHTML = `
+    ${clonedLastMovie}
+    ${heroContents}
+    `
+
+    
+    
+
     const nextBtn = document.querySelector('.nextBtn')
     const dotsBox = document.querySelector('.dotBox')
     
@@ -91,6 +135,7 @@
     const dotCenter = (window.innerWidth / 2) - 86
     dotsBox.style.left=dotCenter + 'px'
 
+    
     // arrow click function
     const arrows = document.querySelectorAll('.arrow')
     arrows.forEach((arrow)=>{
